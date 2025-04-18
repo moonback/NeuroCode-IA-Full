3a499855afb4578bbd4b8d4ee9e7a2065efa9287
Medusa <devoye.maysson@gmail.com>
2025-04-19 01:25:31 +0200

    Update ExpoQrModal.tsx

--- app/components/workbench/ExpoQrModal.tsx
+++ app/components/workbench/ExpoQrModal.tsx
@@ -22,11 +22,10 @@ export const ExpoQrModal: React.FC<ExpoQrModalProps> = ({ open, onClose }) => {
         <div className="border !border-bolt-elements-borderColor flex flex-col gap-5 justify-center items-center p-6 bg-bolt-elements-background-depth-2 rounded-md">
           <div className="i-bolt:expo-brand h-10 w-full"></div>
           <DialogTitle className="text-white text-lg font-semibold leading-6">
-            Preview on your own mobile device
+          Aperçu sur votre propre appareil mobile
           </DialogTitle>
           <DialogDescription className="bg-bolt-elements-background-depth-3 max-w-sm rounded-md p-1 border border-bolt-elements-borderColor">
-            Scan this QR code with the Expo Go app on your mobile device to open your project.
-          </DialogDescription>
+          Scannez ce code QR avec l'application Expo Go sur votre appareil mobile pour ouvrir votre projet.          </DialogDescription>
           <div className="my-6 flex flex-col items-center">
             {expoUrl ? (
               <div className="bg-white p-1 flex flex-col rounded-md justify-center items-center ">
